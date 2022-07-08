e434d3a70597e8abe0268bdab6b76d56ab086d89
Piotr Tomczyk <93157684+piotr-tomczyk@users.noreply.github.com>
2022-07-08 07:13:49 +0200

    Add permissions dictonary and Add logic for sending permissions to the extension

---
+++ src/dict.py
@@ -0,0 +1,15 @@
+PERMISSIONS_ADMIN = {
+        'pause': True,
+        'play': True,
+        'seek': True,
+        'chat': True,
+        'kick': True
+}
+
+PERMISSIONS_USER = {
+        'pause': False,
+        'play': False,
+        'seek': False,
+        'chat': True,
+        'kick': False
+}
