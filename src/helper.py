b88ba6be7e9c146023d20f68b53733b725d14859
Michal <michaljanuszkiewicz6@gmail.com>
2022-09-18 18:56:48 +0200

    --no-edit

---
+++ src/helper.py
@@ -0,0 +1,12 @@
+import uuid
+
+def get_room_id_by_sid(sid, room_dict):
+    for roomId in room_dict:
+        for userId in room_dict[roomId]:
+            if sid == userId:
+                return roomId
+    return "ROOM_NOT_FOUND"
+
+def generate_random_uuid():
+    return str(uuid.uuid4())
+    
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import os
 import socketio
 
 from dict import PERMISSIONS_ADMIN, PERMISSIONS_USER
+from helper import generate_random_uuid, get_room_id_by_sid
 
 sio = socketio.Server(cors_allowed_origins='https://www.netflix.com')
 app = socketio.WSGIApp(sio)
@@ -17,17 +18,17 @@ def connect(sid, environ):
 
 
 @sio.event
-def send_message(sid, data):
-    if data['roomId'] not in room_dict:
+def send_message(sid, message):
+    my_room_id = get_room_id_by_sid(sid, room_dict)
+    if my_room_id == "ROOM_NOT_FOUND":
         return "ROOM_NOT_FOUND"
     sio.emit(
-
         event="receive_message",
         data={
             'from': sid,
-            'content': data['message']
+            'content': message,
         },
-        room=data['roomId'],
+        room=my_room_id,
         skip_sid=sid,
     )
 
@@ -35,65 +36,67 @@ def send_message(sid, data):
 
 
 @sio.event
-def send_video_event(sid, data):
-    print(sid, data)
-    sio.emit(event="receive_video_event", data=data['eventInfo'], room=data['roomId'], skip_sid=sid)
+def send_video_event(sid, eventInfo):
+    print(sid, eventInfo)
+    my_room_id = get_room_id_by_sid(sid, room_dict)
+    if my_room_id == "ROOM_NOT_FOUND":
+        return "ROOM_NOT_FOUND"
+    sio.emit(event="receive_video_event", data=eventInfo, room=my_room_id, skip_sid=sid)
     return "OK"
 
 
 @sio.event
-def join_room(sid, roomId):
-    print(sid, roomId)
-    if roomId not in room_dict:
+def join_room(sid, room_id):
+    print(sid, room_id)
+    if room_id not in room_dict:
         return "ROOM_NOT_FOUND"
 
-    sio.enter_room(sid, roomId)
+    sio.enter_room(sid, room_id)
     
-    room_dict[roomId][sid] = {}
-    room_dict[roomId][sid]['permissions'] = PERMISSIONS_USER.copy()
-    sio.emit(event='permissions', data=room_dict[roomId])
+    room_dict[room_id][sid] = {}
+    room_dict[room_id][sid]['permissions'] = PERMISSIONS_USER.copy()
+    sio.emit(event='permissions', data=room_dict[room_id])
     return "OK"
 
 
 @sio.event
-def create_room(sid, roomId):
-    print(sid, roomId)
-    if roomId in room_dict:
+def create_room(sid):
+    room_id = generate_random_uuid();
+    if room_id in room_dict:
         return "ROOM_ALREADY_EXISTS"
     
-    sio.enter_room(sid, roomId)
+    sio.enter_room(sid, room_id)
     
-    room_dict[roomId] = {
+    room_dict[room_id] = {
         sid: {}
     }
-    room_dict[roomId][sid]['permissions'] = PERMISSIONS_ADMIN.copy()
-    sio.emit(event='permissions', data=room_dict[roomId])
-    return "OK"
+    room_dict[room_id][sid]['permissions'] = PERMISSIONS_ADMIN.copy()
+    sio.emit(event='permissions', data=room_dict[room_id])
+    return room_id
 
 
 @sio.event
-def find_all_users_in_room(sid, roomId):
-    if room_dict[roomId] is None:
+def set_users_permissions(sid, user_permissions):
+    print(user_permissions)
+    my_room_id = get_room_id_by_sid(sid, room_dict)
+    if my_room_id == "ROOM_NOT_FOUND":
         return "ROOM_NOT_FOUND"
-    return room_dict[roomId]
-
-
-@sio.event
-def set_users_permissions(sid, data):
-    print(data)
-    roomId = data["roomId"]
-    userPermissions = data["userPermissions"]
-    if room_dict[roomId] is None:
-        return "ROOM_NOT_FOUND"
-    room_dict[roomId] = userPermissions
-    sio.emit(event='permissions', data=room_dict[roomId])
+    
+    room_dict[my_room_id] = user_permissions
+    sio.emit(event='permissions', data=room_dict[my_room_id])
     return "OK"
 
 @sio.event
 def disconnect(sid):
     print('disconnect ', sid)
-    return "OK"
-
+    my_room_id = get_room_id_by_sid(sid, room_dict)
+    if my_room_id == "ROOM_NOT_FOUND":
+        return "OK"
+    else:
+        # remove user from a dict and update permissions
+        room_dict[my_room_id][sid] = {}
+        sio.emit(event='permissions', data=room_dict[my_room_id]) #TODO: send only to specific room
+        return "OK"
 
 if __name__ == '__main__':
     eventlet.wsgi.server(eventlet.listen(('', int(os.environ.get('PORT', '5000')))), app)
