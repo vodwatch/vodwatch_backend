daf387be4ecc46499a5c91ec646b8a29488a0785
Michal <michaljanuszkiewicz6@gmail.com>
2022-10-16 14:17:59 +0200

    Create scheduler for clearing inactive rooms

---
+++ src/scheduler.py
@@ -0,0 +1,26 @@
+import threading
+import time
+
+import schedule
+import socketio
+
+
+class RoomCleanerScheduler(threading.Thread):
+
+    def __init__(self, thread_id):
+        threading.Thread.__init__(self)
+        self.sio = socketio.Client()
+        self.thread_id = thread_id
+
+    def run(self):
+        print("Starting thread: " + str(self.thread_id))
+        print("Connecting with server ...")
+        self.sio.connect(url="http://localhost:5000", wait_timeout=10)
+        schedule.every(30).minutes.do(self.__job)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+    def __job(self):
+        print("Attempting to remove inactive rooms")
+        self.sio.emit("remove_inactive_rooms")
